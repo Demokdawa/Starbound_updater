750c2910fae2ca05c7a304d3715cd92e5fde658b
Demokdawa <Demokdawa@yahoo.fr>
2017-09-27 14:28:48 +0200

    rpc

---
+++ client.py
@@ -0,0 +1,16 @@
+A_dict = {'hash1' : "jsonobj1","hash2" : "jsonobj2"}
+B_dict = {'hash3' : "jsonobj3","hash2" : "jsonobj2"}
+setA = set(A_dict.keys())
+setB = set(B_dict.keys())
+
+print(setA - setB)
+print(setB - setA)
+
+#are only on a:
+items_only_in_a = list(setA)
+for x in items_only_in_a:
+    del A_dict[x]
+
+items_only_in_b = list(setB)
+for x in items_only_in_b:
+    download(B_dict[x])
