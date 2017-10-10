d38401f4f02d81cf5cfc8b827220af928c3c956d
Demokdawa <Demokdawa@yahoo.fr>
2017-10-11 01:30:21 +0200

    cmm

--- serverhash.py
+++ serverhash.py
@@ -7,42 +7,42 @@ import json
 
 class RouteGuideServicer(starbound_pb2_grpc.RouteGuideServicer):
 
-modPath = "/home/steam/starbound/mods"
+	modPath = "/home/steam/starbound/mods"
 
-def find_all_hash ():
-	mod_list_raw = {}
-	for filename in os.listdir(modPath):
-		if os.path.isdir(modPath + "/" + filename):
-			mod_list_raw[filename] = get_folder_hash(filename)
-		else:
-			mod_list_raw[filename] = get_file_hash(filename)
-	print (mod_list_raw)
-	#print ("Value : %s" %  mod_list_raw.items())
+	def find_all_hash ():
+		mod_list_raw = {}
+		for filename in os.listdir(modPath):
+			if os.path.isdir(modPath + "/" + filename):
+				mod_list_raw[filename] = get_folder_hash(filename)
+			else:
+				mod_list_raw[filename] = get_file_hash(filename)
+		print (mod_list_raw)
+		#print ("Value : %s" %  mod_list_raw.items())
 
-def get_folder_hash (filename):
-	hash = checksumdir.dirhash(modPath + "/" + filename)
-	return hash
+	def get_folder_hash (filename):
+		hash = checksumdir.dirhash(modPath + "/" + filename)
+		return hash
 
-def get_file_hash (filename):
-	openedFile = open(modPath + "/" + filename, 'rb')
-	readFile = openedFile.read()
-	md5Hash = hashlib.md5(readFile)
-	md5Hashed = md5Hash.hexdigest()
-	return md5Hashed
+	def get_file_hash (filename):
+		openedFile = open(modPath + "/" + filename, 'rb')
+		readFile = openedFile.read()
+		md5Hash = hashlib.md5(readFile)
+		md5Hashed = md5Hash.hexdigest()
+		return md5Hashed
 
-def GetFeature(self, request, context):
-  feature = get_feature(self.db, request)
-  if feature is None:
-    return route_guide_pb2.Feature(name="", location=request)
-  else:
-    return feature
+	def GetFeature(self, request, context):
+	  feature = get_feature(self.db, request)
+	  if feature is None:
+	    return route_guide_pb2.Feature(name="", location=request)
+	  else:
+	    return feature
 
-def serve():
-  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  route_guide_pb2_grpc.add_RouteGuideServicer_to_server(
-      RouteGuideServicer(), server)
-  server.add_insecure_port('[::]:50051')
-  server.start()
+	def serve():
+	  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+	  route_guide_pb2_grpc.add_RouteGuideServicer_to_server(
+	      RouteGuideServicer(), server)
+	  server.add_insecure_port('[::]:50051')
+	  server.start()
 
-#export_hash_json()
-find_all_hash()
+	#export_hash_json()
+	find_all_hash()
