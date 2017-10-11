5be216ff331aab6ebffd33628ae40cfb97bf9c2a
Demokdawa <Demokdawa@yahoo.fr>
2017-10-11 16:37:03 +0200

    client fix

--- client.py
+++ client.py
@@ -13,22 +13,23 @@ import starbound_pb2_grpc
 
 channel = grpc.insecure_channel('localhost:50051')
 stub = starbound_pb2_grpc.DictSenderStub(channel)
-
 dict_from_server = stub.send_dict(MyDict)
 
-A_dict = {'hash1' : "jsonobj1","hash2" : "jsonobj2"}
-B_dict = {'hash3' : "jsonobj3","hash2" : "jsonobj2"}
-setA = set(A_dict.keys())
-setB = set(B_dict.keys())
+print (dict_from_server)
 
-print(setA - setB)
-print(setB - setA)
+#A_dict = {'hash1' : "jsonobj1","hash2" : "jsonobj2"}
+#B_dict = {'hash3' : "jsonobj3","hash2" : "jsonobj2"}
+#setA = set(A_dict.keys())
+#setB = set(B_dict.keys())
+
+#print(setA - setB)
+#print(setB - setA)
 
 #are only on a:
-items_only_in_a = list(setA)
-for x in items_only_in_a:
-    del A_dict[x]
+#items_only_in_a = list(setA)
+#for x in items_only_in_a:
+#    del A_dict[x]
 
-items_only_in_b = list(setB)
-for x in items_only_in_b:
-    download(B_dict[x])
+#items_only_in_b = list(setB)
+#for x in items_only_in_b:
+#    download(B_dict[x])
