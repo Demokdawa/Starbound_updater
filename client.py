71ff80e864b4535480e36bf69304e31a75c1840e
Demokdawa <Demokdawa@yahoo.fr>
2017-10-11 16:49:01 +0200

    fix3client

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from pathlib import Path
 from os import path
 from concurrent import futures
@@ -11,12 +12,15 @@ import grpc
 import starbound_pb2
 import starbound_pb2_grpc
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = starbound_pb2_grpc.DictSenderStub(channel)
-dict_from_server = stub.send_dict(MyDict)
+def run ()
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = starbound_pb2_grpc.DictSenderStub(channel)
+    dict_from_server = stub.send_dict(MyDict)
+    print (dict_from_server)
 
-print (dict_from_server)
 
+if __name__ == '__main__':
+    run()
 #A_dict = {'hash1' : "jsonobj1","hash2" : "jsonobj2"}
 #B_dict = {'hash3' : "jsonobj3","hash2" : "jsonobj2"}
 #setA = set(A_dict.keys())
