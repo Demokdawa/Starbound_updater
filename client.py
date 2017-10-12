5879978df3409eb8a3fd6f7728febc1f4f4f6d17
Demokdawa <Demokdawa@yahoo.fr>
2017-10-12 11:52:08 +0200

    fix

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ import grpc
 import starbound_pb2
 import starbound_pb2_grpc
 
-modPath = '/home/steam/starbound/mods'
+modPath = 'C:\Program Files (x86)\Adobe\Acrobat DC'
 
 def get_serv_dict():
     channel = grpc.insecure_channel('163.172.30.174:50051')
@@ -24,7 +24,7 @@ def get_serv_dict():
 def find_all_hash():
     client_dict = {}
     for filename in os.listdir(modPath):
-        if os.path.isdir(modPath + '/' + filename):
+        if os.path.isdir(modPath + '\' + filename):
             client_dict[filename] = \
                 get_folder_hash(filename)
         else:
@@ -33,12 +33,12 @@ def find_all_hash():
     return client_dict
 
 def get_folder_hash(filename):
-    hash = checksumdir.dirhash(modPath + '/'
+    hash = checksumdir.dirhash(modPath + '\'
             + filename)
     return hash
 
 def get_file_hash(filename):
-    openedFile = open(modPath + '/' + filename, 'rb')
+    openedFile = open(modPath + '\' + filename, 'rb')
     readFile = openedFile.read()
     md5Hash = hashlib.md5(readFile)
     md5Hashed = md5Hash.hexdigest()
