91b6fd706606033931f6705a209d5c7df19390cd
Demokdawa <Demokdawa@yahoo.fr>
2017-10-12 11:48:52 +0200

    TestDiff

--- client.py
+++ client.py
@@ -12,19 +12,54 @@ import grpc
 import starbound_pb2
 import starbound_pb2_grpc
 
-channel = grpc.insecure_channel('163.172.30.174:50051')
-stub = starbound_pb2_grpc.DictSenderStub(channel)
-response = stub.send_dict(starbound_pb2.Empty())
-dict_from_server = dict(response.dictionary)
-print(dict_from_server)
+def get_serv_dict():
+    channel = grpc.insecure_channel('163.172.30.174:50051')
+    stub = starbound_pb2_grpc.DictSenderStub(channel)
+    response = stub.send_dict(starbound_pb2.Empty())
+    serv_dict = dict(response.dictionary)
+    return serv_dict
+
+def find_all_hash():
+    client_dict = {}
+    for filename in os.listdir(modPath):
+        if os.path.isdir(modPath + '/' + filename):
+            client_dict[filename] = \
+                get_folder_hash(filename)
+        else:
+            client_dict[filename] = \
+                get_file_hash(filename)
+    return client_dict
+
+def get_folder_hash(filename):
+    hash = checksumdir.dirhash(modPath + '/'
+            + filename)
+    return hash
+
+def get_file_hash(filename):
+    openedFile = open(modPath + '/' + filename, 'rb')
+    readFile = openedFile.read()
+    md5Hash = hashlib.md5(readFile)
+    md5Hashed = md5Hash.hexdigest()
+    return md5Hashed
+
 
 #A_dict = {'hash1' : "jsonobj1","hash2" : "jsonobj2"}
 #B_dict = {'hash3' : "jsonobj3","hash2" : "jsonobj2"}
+
+#client_dict = {'hash1' : "jsonobj1","hash2" : "jsonobj2"}
+#serv_dict = {'hash3' : "jsonobj3","hash2" : "jsonobj2"}
+
 #setA = set(A_dict.keys())
 #setB = set(B_dict.keys())
 
+setClient = set(get_serv_dict().keys())
+setServ = set(client_dict().keys())
+
 #print(setA - setB)
 #print(setB - setA)
+
+print(setClient - setServ)
+print(setServ - setClient)
 
 #are only on a:
 #items_only_in_a = list(setA)
