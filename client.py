59d521c4eb0f771bde2d2712d135c4efa365f62b
Demokdawa <Demokdawa@yahoo.fr>
2017-10-17 16:15:10 +0200

    BIGCHANGES

--- client.py
+++ client.py
@@ -11,10 +11,14 @@ import starbound_pb2
 import starbound_pb2_grpc
 import sys, os
 import shutil
-import requests
+import pysftp
 
 installPath = os.getcwd()
 modPath = os.getcwd() + "\\mods\\"
+remotePath = "/home/steam/starbound/mods/"
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+sftp_serv = pysftp.Connection(host="163.172.30.174", username="root",password="Darkbarjot78", cnopts=cnopts)
 
 def get_serv_dict():
     print("Recupération des informations serveur...")
@@ -60,25 +64,36 @@ def remove_extra_files(target_path, client_dict_input, serv_dict_input):
         else:
             print("Erreur lors de la suppression de" + filename_delete)
 
-def download_extra_files(target_path, client_dict_input, serv_dict_input):
+def download_extra_files(target_path, remote_path, client_dict_input, serv_dict_input, sftp_serv):
     for filename_download, server_hash in serv_dict_input.items():
         client_hash = client_dict_input.get(filename_download)
         if client_hash != server_hash:
             if os.path.splitext(target_path + filename_download)[1] == ".pak":
                 print("Telechargement de " + filename_download)
-                print(download_file(target_path, "https://zenmap.xyz/mods/" + filename_download))
+                print(download_file(target_path, remote_path, filename_download, sftp_serv))
             else :
-                print("taeour")
-                print(filename_download)
+                print("Telechargement de " + filename_download)
+                print(download_folder(target_path, remote_path, filename_download, sftp_serv))
 
-def download_file(target_path, url):
-    downloaded_file = url.split('/')[-1]
-    r = requests.get(url, stream=True)
-    with open(target_path + downloaded_file, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=256):
-            if chunk:
-                f.write(chunk)
-    return downloaded_file
+def download_file(target_path, remote_path, name_to_dl, sftp_serv):
+    local_path_to_dl = target_path + name_to_dl
+    remote_path_from_dl = remote_path + name_to_dl
+    sftp.get(remote_path_from_dl, local_path_to_dl)
+
+def download_folder(target_path, remote_path, name_to_dl, sftp_serv):
+    local_path = target_path + dirpath
+    full_path = main_path + dirpath
+    if not sftp.exists(full_path):
+        return
+    if not os.path.exists(local_path):
+        os.makedirs(local_path)
+
+    dirlist = sftp.listdir(remotepath=full_path)
+    for i in dirlist:
+        if sftp.isdir(full_path + '/' + i):
+            grab_dir_rec(sftp, dirpath + '/' + i)
+        else:
+            grab_file(sftp, dirpath + '/' + i)
 
 if os.path.isfile(installPath + "\\" + "Starbound.exe"):
     print("Dossier officiel starbound detecté !")
@@ -86,7 +101,7 @@ if os.path.isfile(installPath + "\\" + "Starbound.exe"):
     client_dict = find_all_hash(modPath)
     serv_dict = get_serv_dict()
     #remove_extra_files(modPath, client_dict, serv_dict)
-    download_extra_files(modPath, client_dict, serv_dict)
+    download_extra_files(modPath, remotePath, client_dict, serv_dict, sftp_serv)
 else :
     print("Le script n'est pas place dans le dossier Starbound !")
     sleep(3)
--- test.py
+++ test.py
@@ -1,18 +1,35 @@
-import os
-from os import path
+import pysftp, os
+from stat import S_ISDIR
+import pysftp
 
-def download_file(url):
-    local_filename = url.split('/')[-1]
-    # NOTE the stream=True parameter
-    r = requests.get(url, stream=True)
-    with open(local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=254): 
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-                #f.flush() commented by recommendation from J.F.Sebastian
-    return local_filename
-	
-#download_file('https://zenmap.xyz/mods/food_stack.pak')
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
 
-var = os.path.splitext("C:\\Users\\PRAN152\\Documents\\GitHub\\Starbound_updater\\serverhash.py")
-print (var)
+sftp = pysftp.Connection(host="163.172.30.174", username="root",password="Darkbarjot78", cnopts=cnopts)
+
+dirpath = "AnTiHair.pak"
+target_path = "C:\\Users\\PRAN152\\Documents\\GitHub\\Starbound_updater\\mods\\"
+main_path = "/home/steam/starbound/mods/"
+
+def grab_dir_rec(sftp, dirpath):
+    local_path = target_path + dirpath
+    full_path = main_path + dirpath
+    if not sftp.exists(full_path):
+        return
+    if not os.path.exists(local_path):
+        os.makedirs(local_path)
+
+    dirlist = sftp.listdir(remotepath=full_path)
+    for i in dirlist:
+        if sftp.isdir(full_path + '/' + i):
+            grab_dir_rec(sftp, dirpath + '/' + i)
+        else:
+            grab_file(sftp, dirpath + '/' + i)
+			
+def grab_file(sftp, dirpath):
+    local_path = target_path + dirpath
+    full_path = main_path + dirpath
+    sftp.get(main_path + dirpath, local_path)
+			
+#grab_dir_rec(sftp, dirpath)
+grab_file(sftp, dirpath)
