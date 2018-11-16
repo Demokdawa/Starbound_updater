507d50736d7144c158bce43dd9f36c16ff9b11fd
Demokdawa <Demokdawa@yahoo.fr>
2018-11-16 16:31:14 +0100

    ZipCreator - First Clean release

--- zip_creator.py
+++ zip_creator.py
@@ -5,24 +5,27 @@ import shutil
 import pwd
 import grp
 
-# CONFIG-PART | THAT IS THE ONLY LINES YOU HAVE TO MODIFY TO CONFIGURE THE ZIP CREATOR
+# CONFIG-PART | THAT IS THE ONLY LINES YOU HAVE TO MODIFY TO CONFIGURE THE ZIP CREATOR----------------------------------
 
-zip_repo = '/home/starb-ftp/starbound/zips'
-install_path = '/home/starb-ftp/starbound'
+# The folder where the zips files will be stored - make sure the permissions are correctly set
+zip_repo = '/home/starb-ftp/starbound/zips/'
+# The folder where the mod files are located - aka starbound server mod folder
 target_path = '/home/starb-ftp/starbound_server/mods/'
-# Name of user and group to access the FTP files
+# Name of user and group to access the FTP files - make sure the user have the correct permissions
 UserID = "starb-ftp"
 GroupID = "starb-ftp"
+
+# END OF CONFIG-PART ! -------------------------------------------------------------------------------------------------
 
 
 for filename in os.listdir(target_path):
     if os.path.isdir(target_path + filename):
-        zip_file = install_path + "/zips/" + filename + ".zip"
+        zip_file = zip_repo + filename + ".zip"
         if os.path.exists(zip_file):
-            os.remove(install_path + "/zips/" + filename + ".zip")
+            os.remove(zip_repo + filename + ".zip")
         shutil.make_archive(filename, "zip", target_path, filename)
-        shutil.move(os.getcwd() + "/" + filename + ".zip", install_path + "/zips/" + filename + ".zip")
-        os.chown(install_path + "/zips/" + filename + ".zip", pwd.getpwnam(UserID).pw_uid, grp.getgrnam(GroupID).gr_gid)
+        shutil.move(os.getcwd() + "/" + filename + ".zip", zip_repo + filename + ".zip")
+        os.chown(zip_repo + filename + ".zip", pwd.getpwnam(UserID).pw_uid, grp.getgrnam(GroupID).gr_gid)
         print(filename + "   [Archivé] !")
 
 print("Terminé !")
