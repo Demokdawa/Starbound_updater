5bc08392426e62d9f02d5c7310db1e42dafe7987
Demokdawa <Demokdawa@yahoo.fr>
2018-11-16 16:23:36 +0100

    Test updaterPath+

--- zip_creator.py
+++ zip_creator.py
@@ -7,10 +7,9 @@ import grp
 
 # CONFIG-PART | THAT IS THE ONLY LINES YOU HAVE TO MODIFY TO CONFIGURE THE ZIP CREATOR
 
-
+zip_repo = '/home/starb-ftp/starbound/zips'
 install_path = '/home/starb-ftp/starbound'
 target_path = '/home/starb-ftp/starbound_server/mods/'
-#updater_path = '/home/starb-ftp/Starbound_updater'
 # Name of user and group to access the FTP files
 UserID = "starb-ftp"
 GroupID = "starb-ftp"
@@ -18,7 +17,7 @@ GroupID = "starb-ftp"
 
 for filename in os.listdir(target_path):
     if os.path.isdir(target_path + filename):
-        zip_file = install_path + "/zips" + filename + ".zip"
+        zip_file = install_path + "/zips/" + filename + ".zip"
         if os.path.exists(zip_file):
             os.remove(install_path + "/zips/" + filename + ".zip")
         shutil.make_archive(filename, "zip", target_path, filename)
