fa80c7fa40a668b81a60a7e10f72f3975fdb2e71
Demokdawa <Demokdawa@yahoo.fr>
2018-11-21 14:19:36 +0100

    Serverhash threadpool test++

--- serverhash3.py
+++ serverhash3.py
@@ -19,8 +19,6 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 def build_server_dict():
     ret_dict = {}
-    dirtest = os.listdir(mod_path)
-    print(dirtest)
 
     def __add_to_dict(hash_tuple):
             f, h = hash_tuple
@@ -28,22 +26,22 @@ def build_server_dict():
             print("Dictionnaire rempli")
     with Pool(processes=4) as pool:
             # Maybe don't let it as a one-liner, especially if you don't understand it fully
-            pool.map_async(hash_compute, os.listdir(mod_path), callback=__add_to_dict)
+            pool.map.async(hash_compute, os.listdir(mod_path), callback=__add_to_dict)
     print(ret_dict)
 
 
 def hash_compute(filename):
         if os.path.isdir(mod_path + '/' + filename):
-                folder_hash = checksumdir.dirhash(mod_path + '/' + filename)
-                hash_tuple = (filename, folder_hash)
-                print(hash_tuple)
+            folder_hash = checksumdir.dirhash(mod_path + '/' + filename)
+            hash_tuple = (filename, folder_hash)
+            print(hash_tuple)
         else:
-                opened_file = open(mod_path + '/' + filename, 'rb')
-                read_file = opened_file.read()
-                md5_hash = hashlib.md5(read_file)
-                file_hash = md5_hash.hexdigest()
-                hash_tuple = (filename, file_hash)
-                print(hash_tuple)
+            opened_file = open(mod_path + '/' + filename, 'rb')
+            read_file = opened_file.read()
+            md5_hash = hashlib.md5(read_file)
+            file_hash = md5_hash.hexdigest()
+            hash_tuple = (filename, file_hash)
+            print(hash_tuple)
 
 
 if __name__ == '__main__':
