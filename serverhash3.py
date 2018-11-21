cbf4d62cc2126cf9650b4e4d6d1e8213855de2c3
Demokdawa <Demokdawa@yahoo.fr>
2018-11-21 15:47:17 +0100

    Serverhash threadpool test++++++

--- serverhash3.py
+++ serverhash3.py
@@ -16,9 +16,10 @@ grpc_port = '[::]:50051'
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+ret_dict = {}
+
 
 def build_server_dict():
-    ret_dict = {}
 
     def __error_map(n):
         lol = n
@@ -38,17 +39,19 @@ def build_server_dict():
 def hash_compute(filename):
         if os.path.isdir(mod_path + '/' + filename):
             folder_hash = checksumdir.dirhash(mod_path + '/' + filename)
-            hash_tuple = (filename, folder_hash)
-            print(hash_tuple)
-            return hash_tuple
+            ret_dict[filename] = folder_hash
+            #hash_tuple = (filename, folder_hash)
+            #print(hash_tuple)
+            #return hash_tuple
         else:
             opened_file = open(mod_path + '/' + filename, 'rb')
             read_file = opened_file.read()
             md5_hash = hashlib.md5(read_file)
             file_hash = md5_hash.hexdigest()
-            hash_tuple = (filename, file_hash)
-            print(hash_tuple)
-            return hash_tuple
+            ret_dict[filename] = file_hash
+            #hash_tuple = (filename, file_hash)
+            #print(hash_tuple)
+            #return hash_tuple
 
 
 if __name__ == '__main__':
