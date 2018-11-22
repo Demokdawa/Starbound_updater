e3d16e1cd090d30d12aba51afc3ff25e92e608d7
Demokdawa <Darkbarjot78>
2018-11-22 01:10:19 +0100

    Test apply_async

--- serverhash3.py
+++ serverhash3.py
@@ -35,11 +35,11 @@ def build_server_dict():
     # pool = Pool(processes=4)
     pool = mp.Pool()
     for filename in os.listdir(mod_path):
-        pool.apply_async(hash_compute, filename, callback=__add_to_dict, error_callback=__error_map)
+        pool.apply_async(hash_compute, (filename, ), callback=__add_to_dict, error_callback=__error_map)
     pool.close()
     pool.join()
     print("finished")
-    
+
 
 def hash_compute(filename):
         if os.path.isdir(mod_path + '/' + filename):
